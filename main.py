3755460f1877f228da60e47bb5a37b0380022258
sansbacon <eric@erictruett.com>
2021-02-15 19:05:38 -0600

    updated script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-r = requests.get('http://www.dougstats.com/20-21RD.txt')
+r = requests.get('http://www.dougstats.com/20-21RD.txt', timeout=3.5)
 lines = r.text.split('\n')
 headers = lines[0].split()
 data = [dict(zip(headers, line.split())) for line in lines[1:]]
